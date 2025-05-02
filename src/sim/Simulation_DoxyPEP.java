0603f4a6c6284f415b27040a72dcb86cd0634b03
Bhui <Bhui@KI840G8-2.ad.nchecr.unsw.edu.au>
2025-05-02 17:12:24 +1000

    Update project to include class specific to Doxy-PEP model

---
+++ src/sim/Simulation_DoxyPEP.java
@@ -0,0 +1,30 @@
+package sim;
+
+import java.io.IOException;
+import java.util.Properties;
+
+public class Simulation_DoxyPEP extends Simulation_ClusterModelTransmission {	
+	
+	public static void main(String[] args) throws IOException, InterruptedException {
+		final String USAGE_INFO = String.format(
+				"Usage: java %s - PROP_FILE_DIRECTORY "
+						+ "<-export_skip_backup> <-printProgress> <-seedMap=SEED_MAP>\n",
+						Simulation_DoxyPEP.class.getName());
+		if (args.length < 1) {
+			System.out.println(USAGE_INFO);
+			System.exit(0);
+		} else {
+			Simulation_ClusterModelTransmission.launch(args);
+		}
+	}
+	
+	
+	@Override
+	public Abstract_Runnable_ClusterModel_Transmission generateDefaultRunnable(long cMap_seed, long sim_seed, 
+			Properties loaProperties) {				
+		return new Runnable_ClusterModel_Prophylaxis(cMap_seed, sim_seed,
+				baseContactMapMapping.get(cMap_seed), loadedProperties);
+	}
+	
+
+}
